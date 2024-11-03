8df73272c7b19968c33111f27f24e577d354b244
Abhigyan Ranjan <abhi@Abhigyans-MacBook-Air.local>
2024-11-03 13:46:10 +0530

    Kokoeating bananas

--- src/BinarySearch/KokoEatingBananas.java
+++ src/BinarySearch/KokoEatingBananas.java
@@ -7,12 +7,10 @@ public class KokoEatingBananas {
         Arrays.sort(piles);
         int maxSpeed = piles[piles.length-1];
         int minSpeed = 1;
-        int mid  ;
+        int mid;
         while(minSpeed <= maxSpeed){
             mid = minSpeed +(maxSpeed - minSpeed)/2 ;
             long h1 = calculateHforSpeed(piles , mid);
-            System.out.println("mid "+mid);
-            System.out.println("h1 "+h1);
             if(h1 <= h) maxSpeed = mid-1;
             else minSpeed = mid+1;
         }
@@ -21,12 +19,7 @@ public class KokoEatingBananas {
     public long calculateHforSpeed(int[] piles , int speed){
         long h=0;
         for(int i=0; i< piles.length; i++){
-           if(piles[i] % speed == 0){
-               h += piles[i] / speed ;
-           }
-           else {
-               h += (piles[i] / speed) + 1;
-           }
+            h += (piles[i] % speed) == 0 ? (piles[i] / speed) : (piles[i] / speed) + 1;
         }
         return h;
     }
